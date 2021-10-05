ba1034b6ee0cb05412112a5310e3a2f10436ade3
GabrielVSM <gabrielvitorsales@gmail.com>
2021-10-05 10:25:23 -0300

    feat: finish text analyzer

--- 00/text_analyzer/count.py
+++ 00/text_analyzer/count.py
@@ -1,9 +1,12 @@
 def text_analyzer(input: str) -> str:
-    num_characters = 1
-    upper_letters = 0
-    lower_letters = 1
-    punctuation_marks = 0
-    spaces = 0
+    """Return a string with the number of characters and other elements:
+    number of upper_cases, lower_cases, punctuation_marks and spaces"""
+    num_characters = len(input)
+    upper_letters = len([i for i in input if i.isupper()])
+    lower_letters = len([i for i in input if i.islower()])
+    spaces = len([i for i in input if i.isspace()])
+    punctuation_marks = num_characters - (spaces +
+                                          upper_letters + lower_letters)
     return f"""The text contains {num_characters} characters:\n
 - {upper_letters} upper letters\n
 - {lower_letters} lower letters\n
